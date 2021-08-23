1da30b588f72406d504881fb33ba2c8d536db669
Md. Mizanur <mrmizanbd97@gmail.com>
2021-08-23 12:10:54 +0600

    initial commit

---
+++ src/main/java/com/FleetApp/ApplicationController.java
@@ -0,0 +1,12 @@
+package com.FleetApp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ApplicationController {
+    @GetMapping("/index")
+    public String getHome(){
+        return "index";
+    }
+}
---
+++ src/main/java/com/FleetApp/FleetAppApplication.java
@@ -0,0 +1,14 @@
+package com.FleetApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetAppApplication.class, args);
+		System.out.println("project runing..");
+	}
+
+}
