a7f0dca1e0da1032c96f9aeda0e125ec13b6052c
Md. Mizanur <mrmizanbd97@gmail.com>
2021-08-24 19:35:34 +0600

    all service created

--- src/main/java/com/FleetApp/service/MyUserDetailsService.java
+++ src/main/java/com/FleetApp/service/MyUserDetailsService.java
@@ -3,5 +3,5 @@ package com.FleetApp.service;
 import org.springframework.stereotype.Service;
 
 @Service
-public class Client {
+public class MyUserDetailsService {
 }
---
+++ src/main/java/com/FleetApp/service/VehicleMaintainance.java
@@ -0,0 +1,7 @@
+package com.FleetApp.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class VehicleMaintainance {
+}
